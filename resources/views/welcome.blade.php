e0ec061daa84a15d9806438116b913912275c4e4
aliff <aliff.rosli96@gmail.com>
2022-02-08 20:14:21 +0800

    adjust session message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,8 +45,14 @@
             </div>
         </nav>
         <!-- Masthead-->
+        
         <header class="masthead">
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                @if ( session()->has('alert-message'))
+                    <div class="alert {{ session()->get('alert-type') }}">
+                        {{ session()->get('alert-message') }}
+                    </div>
+                @endif
                 <div class="d-flex justify-content-center">
                     <div class="text-center">
                         <h1 class="mx-auto my-0 text-uppercase">Computer Club System</h1>
