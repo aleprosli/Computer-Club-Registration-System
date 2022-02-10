4920fb5b7f98d6d4aa646faa9aa15f8ee76b27f5
Aleprosli <aliff.rosli96@gmail.com>
2022-02-10 12:39:52 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,16 @@ Route::get('/registration', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::post('register/user', [App\Http\Controllers\UserController::class, 'store'])->name('register:user');
+Route::post('register/user', [App\Http\Controllers\StudentController::class, 'store'])->name('register:user');
+
+Route::get('student/index', [App\Http\Controllers\StudentController::class, 'index'])->name('student:index');
+Route::get('student/index/approve/{student}', [App\Http\Controllers\StudentController::class, 'approve'])->name('student:approve');
+Route::get('student/index/reject/{student}', [App\Http\Controllers\StudentController::class, 'reject'])->name('student:reject');
+Route::get('student/index/show/{student}', [App\Http\Controllers\StudentController::class, 'show'])->name('student:show');
+Route::get('student/destroy/{student}', [App\Http\Controllers\StudentController::class, 'destroy'])->name('student:destroy');
+
+
+
+
+
 
