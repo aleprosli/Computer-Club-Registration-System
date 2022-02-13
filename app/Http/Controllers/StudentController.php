ce97af85765fe545cc9ca7a9b2c71e1e60c31621
aliff <aliff.rosli96@gmail.com>
2022-02-13 22:28:51 +0800

    add pagination / search keyword by identification number / update method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,10 +8,17 @@ use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $students = Student::all();
 
+        if($request->keyword){
+            $students = Student::query()
+                        ->where('ic','LIKE','%'.$request->keyword.'%')
+                        ->paginate(3);
+        }else{
+            $students =  Student::paginate(10);
+        }
         return view('registration.admin.index',compact('students'));
     }
 
@@ -42,6 +49,33 @@ class StudentController extends Controller
         return view('registration.admin.show',compact('student'));
     }
 
+    public function update(Request $request, Student $student)
+    {
+        $student->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'username' => $request->username,
+            'ic' => $request->ic,
+            'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'age' => $request->age,
+            'phone_number' => $request->phone_number,
+            'gender' => $request->gender,
+            'interest' => $request->interest,
+            'class' => $request->class,
+        ]);
+
+        return redirect()->route('student:index')->with([
+            'alert-type' => 'alert-success',
+            'alert-message' => 'Student information has been updated'
+        ]);
+    }
+
+    public function edit(Request $request,Student $student)
+    {
+        return view('registration.admin.edit',compact('student'));
+    }
+
     public function approve(Request $request,Student $student)
     {
         $student->update([
@@ -61,7 +95,7 @@ class StudentController extends Controller
         ]);
 
         return redirect()->route('student:index')->with([
-            'alert-type' => 'alert-success',
+            'alert-type' => 'alert-danger',
             'alert-message' => 'This student has been rejected'
         ]);
     }
@@ -71,7 +105,7 @@ class StudentController extends Controller
         $student->delete();
 
         return redirect()->route('student:index')->with([
-            'alert-type' => 'alert-success',
+            'alert-type' => 'alert-danger',
             'alert-message' => 'This student has been deleted'
         ]);
     }
